898b7d0d782feb057d2df1133969c306a6ff4cbd
Ahmed-boighdady <147153759+AhmedSumaiya@users.noreply.github.com>
2023-10-15 10:23:30 +0900

    2nd

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Get the button element by its ID
-const changeColorButton = document.getElementById('change-color-button');
 
 // Function to generate a random color
 function getRandomColor() {
@@ -10,15 +9,3 @@ function getRandomColor() {
     }
     return color;
 }
-
-// Function to change the background color when the button is clicked
-function changeBackgroundColor() {
-    // Get a random color
-    const randomColor = getRandomColor();
-
-    // Set the background color of the body to the random color
-    document.body.style.backgroundColor = randomColor;
-}
-
-// Attach the changeBackgroundColor function to the button's click event
-changeColorButton.addEventListener('click', changeBackgroundColor);
